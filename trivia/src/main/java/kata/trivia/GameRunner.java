610fe1332b75e3f8240902534f52c2619fb9d1f9
Ben Wu <wubin28@gmail.com>
2014-01-17 19:30:32 +0800

    Noted all pieces of the major logic which are not going to be modified.

--- trivia/src/main/java/kata/trivia/GameRunner.java
+++ trivia/src/main/java/kata/trivia/GameRunner.java
@@ -21,18 +21,12 @@ public class GameRunner {
         Random rand = new Random();
 
         do {
-
             aGame.roll(rand.nextInt(5) + 1);
-
             if (rand.nextInt(9) == 7) {
                 notAWinner = aGame.wrongAnswer();
             } else {
                 notAWinner = aGame.wasCorrectlyAnswered();
             }
-
-
-
         } while (notAWinner);
-
     }
 }

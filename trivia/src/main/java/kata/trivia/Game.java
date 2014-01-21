fc6bb8e12c7bf44fa29ed71ccaeb8b5e0af943b8
Ben Wu <wubin28@gmail.com>
2014-01-21 18:29:00 +0800

    Wrote expected code of encapsulating System.out.println() in class MyLogger.log().

--- trivia/src/main/java/kata/trivia/Game.java
+++ trivia/src/main/java/kata/trivia/Game.java
@@ -26,7 +26,7 @@ public class Game
         purses[howManyPlayers()] = 0;
         inPenaltyBox[howManyPlayers()] = false;
 
-        System.out.println(playerName + " was added");
+        MyLogger.log(playerName + " was added");
         System.out.println("They are player number " + players.size());
         return true;
     }

a47200f6d2fac9dc37aee4947205ed59447e162c
Ben Wu <wubin28@gmail.com>
2014-01-21 18:30:42 +0800

    Implemented method MyLogger.log().

---
+++ trivia/src/main/java/kata/trivia/MyLogger.java
@@ -0,0 +1,14 @@
+package kata.trivia;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 14-1-21
+ * Time: 下午6:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyLogger {
+    public static void log(String message) {
+        System.out.println(message);
+    }
+}

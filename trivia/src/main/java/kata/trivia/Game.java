44b3f32ea296dfbbd7e07a4710b44ad665cd67e6
Ben Wu <wubin28@gmail.com>
2014-01-21 17:10:18 +0800

    Added method call getName() whereas a player's name needed.

--- trivia/src/main/java/kata/trivia/Game.java
+++ trivia/src/main/java/kata/trivia/Game.java
@@ -45,17 +45,17 @@ public class Game
             if (roll % 2 != 0) {
                 isGettingOutOfPenaltyBox = true;
 
-                System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
+                System.out.println(players.get(currentPlayer).getName() + " is getting out of the penalty box");
                 places[currentPlayer] = places[currentPlayer] + roll;
                 if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
-                System.out.println(players.get(currentPlayer)
+                System.out.println(players.get(currentPlayer).getName()
                         + "'s new location is "
                         + places[currentPlayer]);
                 System.out.println("The category is " + questionMaker.currentCategory(this));
                 questionMaker.askQuestion(this);
             } else {
-                System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
+                System.out.println(players.get(currentPlayer).getName() + " is not getting out of the penalty box");
                 isGettingOutOfPenaltyBox = false;
             }
 
@@ -64,7 +64,7 @@ public class Game
             places[currentPlayer] = places[currentPlayer] + roll;
             if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
-            System.out.println(players.get(currentPlayer)
+            System.out.println(players.get(currentPlayer).getName()
                     + "'s new location is "
                     + places[currentPlayer]);
             System.out.println("The category is " + questionMaker.currentCategory(this));
@@ -79,7 +79,7 @@ public class Game
             if (isGettingOutOfPenaltyBox()) {
                 System.out.println("Answer was correct!!!!");
                 purses[currentPlayer]++;
-                System.out.println(players.get(currentPlayer)
+                System.out.println(players.get(currentPlayer).getName()
                         + " now has "
                         + purses[currentPlayer]
                         + " Gold Coins.");
@@ -101,7 +101,7 @@ public class Game
 
             System.out.println("Answer was corrent!!!!");
             purses[currentPlayer]++;
-            System.out.println(players.get(currentPlayer)
+            System.out.println(players.get(currentPlayer).getName()
                     + " now has "
                     + purses[currentPlayer]
                     + " Gold Coins.");
@@ -116,7 +116,7 @@ public class Game
 
     public boolean wrongAnswer(){
         System.out.println("Question was incorrectly answered");
-        System.out.println(players.get(currentPlayer)+ " was sent to the penalty box");
+        System.out.println(players.get(currentPlayer).getName() + " was sent to the penalty box");
         inPenaltyBox[currentPlayer] = true;
 
         currentPlayer++;

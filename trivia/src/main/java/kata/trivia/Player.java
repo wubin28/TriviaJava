9fbbe4b2f199a6e798d3369c0f41870c60692d63
Ben Wu <wubin28@gmail.com>
2014-01-21 16:52:24 +0800

    Created class Player.

---
+++ trivia/src/main/java/kata/trivia/Player.java
@@ -0,0 +1,11 @@
+package kata.trivia;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 14-1-21
+ * Time: 下午4:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+}
